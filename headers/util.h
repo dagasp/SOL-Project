a83816b9eade6e1ad307d4b2674f43ed4e73693c
David <davyx96@gmail.com>
2021-06-30 16:45:08 +0200

    fixed definitions

--- headers/util.h
+++ headers/util.h
@@ -181,7 +181,21 @@ config_file *read_config(char *config); // da implementare
  *   \retval  0   se durante la lettura da fd leggo EOF
  *   \retval size se termina con successo
  */
-static inline int readn(long fd, void *buf, size_t size);
+static inline int readn(long fd, void *buf, size_t size) {
+    size_t left = size;
+    int r;
+    char *bufptr = (char*)buf;
+    while(left>0) {
+	if ((r=read((int)fd ,bufptr,left)) == -1) {
+	    if (errno == EINTR) continue;
+	    return -1;
+	}
+	if (r == 0) return 0;   // EOF
+        left    -= r;
+	bufptr  += r;
+    }
+    return size;
+}
 
 
 /** Evita scritture parziali
@@ -190,7 +204,21 @@ static inline int readn(long fd, void *buf, size_t size);
  *   \retval  0   se durante la scrittura la write ritorna 0
  *   \retval  1   se la scrittura termina con successo
  */
-static inline int writen(long fd, void *buf, size_t size);
+static inline int writen(long fd, void *buf, size_t size) {
+    size_t left = size;
+    int r;
+    char *bufptr = (char*)buf;
+    while(left>0) {
+	if ((r=write((int)fd ,bufptr,left)) == -1) {
+	    if (errno == EINTR) continue;
+	    return -1;
+	}
+	if (r == 0) return 0;  
+        left    -= r;
+	bufptr  += r;
+    }
+    return 1;
+}
 
 
 #endif /* _UTIL_H */

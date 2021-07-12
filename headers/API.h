6f2f94c5380831ffc3c67a0ecf701f487a62db34
David <davyx96@gmail.com>
2021-07-12 23:01:33 +0200

    aggiunta api per scrivere da file in locale

--- headers/API.h
+++ headers/API.h
@@ -26,37 +26,11 @@
 #define O_LOCK 1
 #define O_CREATE_OR_O_LOCK 2
 
-enum OP_CODE {
-    OPENFILE = 0,
-    READFILE = 1,
-    READNFILES = 2,
-    WRITEFILE = 3,
-    APPENDTOFILE = 4,
-    CLOSEFILE = 5,
-    CLOSECONNECTION = 6
-};
 
-typedef struct client_operations {
-    char pathname[MAX_PATH];
-    char data[MAX_FILE_SIZE];
-    char dirname [MAX_PATH];
-    long client_desc;
-    unsigned int op_code;
-    int flags;
-    int feedback;
-    int files_to_read;
-    size_t size;
-} client_operations;
-
-typedef struct server_reply {
-    char pathname[MAX_PATH];
-    char data[MAX_FILE_SIZE];
-    unsigned int size;
-    unsigned int reply_code;
-    int n_files_letti;
-} server_reply;
 
 /*API d'appoggio al Client*/
+
+int writeToFile(char *pathname, char *content, const char *dirname);
 
 int openConnection(const char *sockname, int msec, const struct timespec abstime);
 
@@ -68,12 +42,13 @@ int readFile(const char* pathname, void** buf, size_t* size);
 
 int readNFiles(int N, const char *dirname);
 
-int writeFile(const char *pathname, const char *dirname);
+//int writeFile(const char *pathname, const char *dirname);
 
 int appendToFile(const char *pathname, void *buf, size_t size, const char *dirname);
 
 int closeFile(const char *pathname);
 
-int writeToFile(char *pathname, char *content, const char *dirname);
+int writeFile (char *file_name);
+
 
 #endif

4fd968d6635dff5f3c34b240d73d993d36b3c7e9
David <davyx96@gmail.com>
2021-07-12 23:04:29 +0200

    aggiunto test per algoritmo di rimpiazzamento

--- script/test2.sh
+++ script/test2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Welcome to Test2"; 
-echo -e "4\n400\n10\ncs_sock" > ./test/config.txt #Setta i parametri del file config.txt
+echo -e "4\n1000000\n10\ncs_sock" > ./test/config.txt #Setta i parametri del file config.txt
 
 SERVER_CMD=./file_storage_server #Eseguibile del server
 CLIENT_CMD=./client #Eseguibile del client
@@ -11,13 +11,10 @@ valgrind --leak-check=full --show-leak-kinds=all $SERVER_CMD & #Avvia il server 
 pid=$!
 sleep 2s
 
-#Proverà ad effettuare l'append al file con 10 client diversi
-for i in {1..10}; do
-    $CLIENT_CMD -f cs_sock* -r pippo -p
+#Prova a scrivere 15 files diversi
+for i in {1..15}; do
+    $CLIENT_CMD -f cs_sock -w ./files/file$i.txt
 done
-
-
-
 
 sleep 2s
 
